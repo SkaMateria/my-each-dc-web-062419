3410276bdcee15de067bb117a93c914cd68f214c
Carlos Velazquez <skamateria@gmail.com>
2019-06-14 18:53:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,7 @@
 collection = [1, 2, 3, 4]
 
 def my_each(collection)
+<<<<<<< HEAD
   if block_given?
   i = 0
   while i < collection.length
@@ -8,6 +9,13 @@ def my_each(collection)
     i += 1
   end
   collection
+=======
+  i = 0
+  while i < collection.length
+    yield collection[i]
+    i += 1
+  end
+>>>>>>> ab7dc77f2c47e9990e11188af981cf02f7bd0bb8
 end
 end
 my_each(collection)
